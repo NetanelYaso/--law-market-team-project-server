48fcf74a09bed2b7d84e72c4d271424f69853499
chen-semu <chensemu@gmail.com>
2023-01-11 20:06:28 +0200

    adding department creating with sub

--- validation/departmentValidation.js
+++ validation/departmentValidation.js
@@ -1,13 +1,12 @@
 const Joi = require("@hapi/joi");
 
 module.exports =  validateDeparment = (department) => {
-    console.log(department,"fdfdfdf");
     const schema = Joi.object({
         name: Joi.string().min(5).required(),
         active: Joi.boolean().required(),
         coverImage: Joi.required(),
         // lawyers: Joi.required(),
-        categories:Joi.required(),
+        subDepartments:Joi.object()
     });
     const{error}=schema.validate(department, {abortEarly:false});
     return {error}
